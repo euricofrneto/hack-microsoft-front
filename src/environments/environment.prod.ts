bc0fb5b9f1606381c9ba5f8fb4f3df556f597cb2
Henrique Siqueira <h.enrique.siq@hotmail.com>
2022-11-30 22:08:17 -0300

    Wip

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     env: 'local',
     // base_url: 'http://localhost:8000',
-    base_url: 'http://educaimicrosoft.online',
+    base_url: 'https://henriqueonline.com.br',
 };
